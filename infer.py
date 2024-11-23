80a05f77bfcb47c9e783a5d79cdeaf25fa4eba24
Revlis15 <bopbin15@gmail.com>
2024-11-23 23:00:14 +0700

    update

--- infer.py
+++ infer.py
@@ -103,7 +103,7 @@ def main():
     args = parse_args()
     
     try:
-        model = BKAIPolypModel.load_from_checkpoint('checkpoints-20241123T123919Z-001/checkpoints/model/model.ckpt')
+        model = BKAIPolypModel.load_from_checkpoint('checkpoints-20241123T153032Z-001\checkpoints\model\model.ckpt')
         model.eval()
         
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
